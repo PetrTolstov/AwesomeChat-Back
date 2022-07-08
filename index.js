fd35f87beba6187e41c658b3217ca0a4e7a2060d
PetrTolstov <57655896+PetrTolstov@users.noreply.github.com>
2022-07-08 22:21:09 +0300

    f

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require("socket.io");
+const cors = require("cors")
+
+app.use(cors())
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(`user: ${socket.id}`)
+    socket.on('send_message', (data) => {
+        data.user = socket.id
+        console.log(data)
+        socket.broadcast.emit("receive_message", data)
+    });
+});
+
+server.listen(3001, () => {
+    console.log('listening on *:3000');
+});
+
+/*
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+ */
